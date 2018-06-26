fd744b64d36f36ed34ca8e7bf37396bbcbdaae72
ac257 <acor2357@gmail.com>
2018-06-26 09:52:07 +0100

    modified data.R - AC

--- R/data.R
+++ R/data.R
@@ -7,3 +7,17 @@ sw.data = function( )
   data = data.table( pValue = runif( 1e4, 0 , 1 ) )
   return( data )
 }
+
+#' Results of performing a univariate test of association at each
+#' node in the tree where data is observed.
+#'
+#'
+'ABO.1df.res'
+
+#' Likelihood surfaces at each node in the tree with observed data
+#' for TreeWAS analysis
+#' 
+'ABO.lk.surfs'
+
+
+    
