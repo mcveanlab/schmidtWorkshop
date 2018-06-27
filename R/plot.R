ac18cb5423caebd015aec3f213c4c2e84cd175ba
ac257 <acor2357@gmail.com>
2018-06-27 11:46:05 +0100

    modified qq plot func - AC

--- R/plot.R
+++ R/plot.R
@@ -4,19 +4,25 @@
 # assume the data is normally distributed, convert to normal and
 # plot against theoretical z-score
 ###################################################################/
-sw.plot.qq = function( data )
+sw.plot.qq = function( data, annot.col = 'meaning' )
 {
-  # convert the pValues to a normal standard error
-  data[ , zData := qnorm( pValue )]
+    require(plotly)
 
-  # now add theoritical z vales
-  data = data[ order( pValue ) ]
-  data[ , zTheor := qnorm( (1:data[,.N ] - 0.5 )/data[,.N ] ) ]
+    ## remove missing data
+    data <- na.omit(data)
+    
+    ## convert the pValues to a normal standard error
+    data[ , zData := qnorm( pValue )]
 
-  # make plot
-  plot = plot_ly( data, x = ~zTheor, y = ~zData, type = "scatter", mode = "markers", name = "data" )
-  plot = layout( plot, xaxis = list( title = "theoreitcal z-score" ), yaxis = list( title = "data z-score" ), title = "Q-Q Plot" )
-  plot = add_lines( plot, x = ~zData, y = ~zData, name = "null" )
+    ## now add theoritical z vales
+    data = data[ order( pValue ) ]
+    data[ , zTheor := qnorm( (1:data[,.N ] - 0.5 )/data[,.N ] ) ]
+  
+
+    ## make plot
+    plot = plot_ly( data, x = ~zTheor, y = ~zData, type = "scatter", mode = "markers", name = "data", text = data[[annot.col]], hoverinfo = "text" )
+    plot = layout( plot, xaxis = list( title = "theoreitcal z-score" ), yaxis = list( title = "data z-score" ), title = "Q-Q Plot" )
+    plot = add_lines( plot, x = ~zData, y = ~zData, name = "null" )
 
   return( plot )
 }
